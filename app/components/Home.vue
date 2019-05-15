2e5256014c9e62f4c8f31521371e1624265b1fcc
Jeremy Jackson <jeremy@buzzit.ca>
2019-05-15 16:59:45 -0700

    4.3.1 - Empty Home Component

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -6,19 +6,7 @@
 
         <StackLayout>
 
-            <ScrollView>
-                <StackLayout>
-                    <Label class="sofia" text="Some" />
-                    <Label text="Text" />
-                    <Label text="To" />
-                    <Label text="Fill" />
-                    <Label text="The" />
-                    <Label text="Space" />
-                    <Label text="To" />
-                    <Label text="Force" />
-                    <Label text="Scrolling" />
-                </StackLayout>
-            </ScrollView>
+            
             
         </StackLayout>
 
@@ -29,41 +17,14 @@
     export default {
         data() {
             return {
-                items: [
-                    { text: 'Patty' },
-                    { text: 'Jessica' },
-                    { text: 'Mackenzie' },
-                    { text: 'Sophia' },
-                    { text: 'Dapper' },
-                    { text: 'Sam' },
-                    { text: 'Andrew' },
-                    { text: 'Coral' },
-                    { text: 'Nadia' },
-                    { text: 'Moma' },
-                    { text: 'Carl' }
-                ]
+                
             }
         },
         computed: {
-            anotherMessage() {
-                return "Beautiful View";
-            },
-            message() {
-                return "My Application!";
-            }
+            
         },
         methods: {
-            openDialog() {
-                prompt({
-                    title: "Prompt Title",
-                    message: "Prompt Body",
-                    okButtonText: "Submit",
-                    cancelButtonText: "Cancel",
-                    defaultText: "Initial Text",
-                }).then(result => {
-                    console.log('Results');
-                });
-            }
+            
         }
     };
 </script>
@@ -72,17 +33,5 @@
     // Start custom common variables
     @import '../app-variables';
     // End custom common variables
-    Label {
-        border-width:1;
-        border-color:red;
-        height: 100;
-    }
-    // Custom styles
-    .fa {
-        color: red;
-    }
-
-    .info {
-        font-size: 30;
-    }
+    
 </style>
