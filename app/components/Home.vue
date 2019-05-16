7b0c910adfc437266dc5ee754dc2a52bee9b52ab
Jeremy Jackson <jeremy@buzzit.ca>
2019-05-15 17:03:16 -0700

    4.4 - Section 4 Completed

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -4,11 +4,24 @@
             <NavigationButton text="Go back" android.systemIcon="ic_menu_back" @tap="goBack" />
         </ActionBar>
 
-        <StackLayout>
+        <GridLayout
+        rows="200,*">
 
-            
-            
-        </StackLayout>
+            <Image 
+                src="~/Images/logo.png" />
+            <StackLayout
+                row="1">
+                <Button 
+                    text="Button" 
+                    @tap="increaseCounter"
+                    :class="buttonBackground" />
+                <TextField v-model="currentCounter" hint="Something" />
+                <Button
+                    text="Prompt"
+                    @tap="askUser" />
+                <TextField v-model="currentAnswer" hint="Answer" />
+            </StackLayout>
+        </GridLayout>
 
     </Page>
 </template>
@@ -17,14 +30,29 @@
     export default {
         data() {
             return {
-                
+                currentCounter: 0,
+                currentAnswer: ""
             }
         },
         computed: {
-            
+            buttonBackground() {
+                if (this.currentCounter > 10) {
+                    return "red-background";
+                } else {
+                    return "";
+                }
+            }
         },
         methods: {
-            
+            increaseCounter() {
+                this.currentCounter++;
+            },
+            askUser() {
+                prompt('What is your name?')
+                    .then(result => {
+                        this.currentAnswer = result.text;
+                    });
+            }
         }
     };
 </script>
@@ -33,5 +61,8 @@
     // Start custom common variables
     @import '../app-variables';
     // End custom common variables
-    
+    .red-background {
+        background-color: red;
+        color: black;
+    }
 </style>
